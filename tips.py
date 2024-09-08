b3ab6943ba088f99e41d082ec57040ff3e1e8130
claire <clairemwas20@gmail.com>
2024-09-08 18:21:47 +0300

    rounding off the final result

--- tips.py
+++ tips.py
@@ -2,5 +2,5 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give?10, 12 or 15?"))
 people = int(input("How many to split the bill?"))
-pay= (bill + (tip/100*bill))/people
+pay= round((bill + (tip/100*bill))/people)
 print(f"Each person will pay {pay}")
