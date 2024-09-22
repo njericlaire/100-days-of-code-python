f2ab135a9bf97fab3017d53dbdd7df2140cb5780
claire <clairemwas20@gmail.com>
2024-09-22 10:52:32 +0300

    finishing highlow project

--- HighLow/HighLow.py
+++ HighLow/HighLow.py
@@ -18,26 +18,37 @@ def compare(guess,A,B):
             return False
 
 
-
-continue_playing=True
+def format_data(account):
+    A_name=account['name']
+    A_desc=account['description']
+    A_location=account['country']
+    return f" {A_name}, a {A_desc} from {A_location}"
 score=0
+continue_playing=True
+B = random.choice(data)
+
+
 while continue_playing:
     
     print(logo)
 
     #get random data from the  data
-    A=random.choice(data)
-    print(f"Compare A: {A['name']}, a {A['description']} from {A['country']}")
-    print(vs)
+    A=B
     B=random.choice(data)
-    print(f"Against B: {B['name']}, a {B['description']} from {B['country']}")
+    if A==B:
+        B=random.choice(data)
+    print(f"Compare A:{format_data(A)}")
+    print(vs)
+    print(f"Against  B:{format_data(B)}")
    
-    choice=input("Who has more followers. Type 'A' or 'B' ")
+    choice=(input("Who has more followers. Type 'A' or 'B' ")).lower()
+    A_follower=A["follower_count"]
+    B_follower=B["follower_count"]
     result=compare(choice,A,B)
     if result:
         score+=1
-        print(f"Your score is {score}")
-        continue_playing=result
+        print(f"You're right! Current score {score}")
+        
     else:
-        print(f"Your score is {score}")
-        continue_playing=result
+        print(f"Sorry, that's wrong. Final score: {score}.")
+        continue_playing=False
